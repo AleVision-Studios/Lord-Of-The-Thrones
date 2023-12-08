a6029c90e3f377946a8306d33b0c6ebde074693e
Douglas <doggek@hotmail.se>
2023-12-08 11:56:34 +0100

    Began work on startscreen
    The play button doesnt work atm but doesnt interfere with the project

---
+++ LordOfTheThrones/Script/StartScreen.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class StartScreen : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+        GetNode<Button>("CenterContainer/VBoxContainer/Play").Pressed +=
+            () => GetNode<SceneLoader>("/root/SceneLoader").ChangeToScene("game.tscn");
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public void ExitGame()
+    {
+        GetTree().Quit();
+    }
+}
